63a44d14db6fdab31dc0c67d36b1044a6ff8a5bf
MivelDevelopment <milosveljkovic1992@gmail.com>
2023-01-19 14:11:01 +0100

    add conditional route for dashboard

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -1,13 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { Homepage, RegisterPage } from 'components';
 
 export const PageRoutes = () => {
+	const cookies = document.cookie.split(';');
+	const uid = cookies.find((cookie) => cookie.includes('_uid'));
+
 	return (
 		<Routes location={location}>
 			<Route path="/" element={<Homepage />} />
 			<Route path="/login" element={<h1>Login</h1>} />
 			<Route path="/register" element={<RegisterPage />} />
+			<Route
+				path="/dashboard"
+				element={
+					uid ? <h1>Dashboard</h1> : <Navigate to="/login" replace={true} />
+				}
+			/>
 			<Route path="/*" element={<h1>Not found</h1>} />
 		</Routes>
 	);
