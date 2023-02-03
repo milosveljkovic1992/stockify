b6f6105c06ec4f0398b6ec76f0ff9436e7eaff3b
MivelDevelopment <milosveljkovic1992@gmail.com>
2023-02-04 00:38:01 +0100

    remove redundant interceptor

--- src/axios/interceptors.ts
+++ src/axios/interceptors.ts
@@ -3,12 +3,6 @@ import axios from 'axios';
 axios.interceptors.request.use(
 	(config) => {
 		config.withCredentials = true;
-
-		const isAuth = config.url?.includes(`/auth`);
-		if (!isAuth) {
-			axios.get(`/auth/token`);
-		}
-
 		return config;
 	},
 	(error) => {
